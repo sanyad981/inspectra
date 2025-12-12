22e2cbdcbbb1a92249da8f1d1061c32052f9dbe5
Sanya <sanyad981@gmail.com>
2025-12-12 18:46:03 +0530

    chore(db): setup the prisma and tried the test migration

---
+++ lib/db.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from "./generated/prisma/client";
+import {PrismaPg} from "@prisma/adapter-pg";
+ 
+
+const adapter = new PrismaPg({
+    connectionString : process.env.DATABASE_URL,
+});
+
+ const prismaClientSignleton = () => {
+     return new PrismaClient({adapter})
+ }
+
+
+ declare const globalThis: {
+    prismaGlobal: ReturnType<typeof prismaClientSignleton> ;
+ } & typeof global ;
+
+ const prisma = globalThis.prismaGlobal || prismaClientSignleton();
+
+ if(process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+
+ export default prisma;
